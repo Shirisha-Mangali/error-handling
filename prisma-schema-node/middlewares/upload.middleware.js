23c1bd7c7e67eb4fb3df7b855a9de1629454d12a
Shirisha-Mangali <shirisha.mangali@cognine.com>
2025-12-26 19:48:30 +0530

    commit for img upload

---
+++ prisma-schema-node/middlewares/upload.middleware.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+import path from "path";
+
+//storage configuration 
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"uploads/doctors");
+    },
+    filename:(req,file,cb)=>{
+        const uniqueName=Date.now()+"-"+Math.round(Math.random()*1e9);
+        cb(null,uniqueName+path.extname(file.originalname));
+    },
+});
+//file validation
+const fileFilter=(req,file,cb)=>{
+    const allowedTypes=["image/jpeg","image/png"];
+    if(!allowedTypes.includes(file.mimetype)){
+        cb(new Error("only JPG and PNG allowed"),false);
+    }else{
+        cb(null,true);
+    }
+}
+//multer instance 
+export const uploadDoctorImage=multer({
+    storage,
+    fileFilter,
+    limits:{
+        fileSize:2*1024*1024,//2mb
+    },
+});
