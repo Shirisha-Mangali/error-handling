c4c0b4a51d7ca8db4cbcbe9ccef5bee83c52877e
Shirisha-Mangali <shirisha.mangali@cognine.com>
2025-12-30 11:59:29 +0530

    Fix Render port and env handling

--- prisma-schema-node/server.js
+++ prisma-schema-node/server.js
@@ -57,11 +57,20 @@ app.get("/health", (req, res) => {
 
 //app.listen(3000, () => console.log("Server running on "));
 
+// if (process.env.NODE_ENV !== "test") {
+//   app.listen(3000, () => {
+//     console.log("Server running on port 3000");
+//   });
+// }
+
+const PORT = process.env.PORT || 3000;
+
 if (process.env.NODE_ENV !== "test") {
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
+
 
 
 
